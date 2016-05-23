49026cc5e5d0b041f69588390f733a49a0f11edc
Barys Marukhin <delfinchick90@gmail.com>
2016-05-23 16:39:51 +0200

    responsive layout finished

--- components/scripts/scrollnav.js
+++ components/scripts/scrollnav.js
@@ -19,64 +19,58 @@ $(function() {
   });//on click
 
 //Highlight navigation
-$(window).scroll(function(){
-  var windowpos = $(window).scrollTop() + topoffset;
-  $('header nav li a').removeClass('active');
-  $('.nav_right__item').removeClass('activve');
-
-  if (windowpos >= $('#maingallery').offset().top){
+  $(window).scroll(function(){
+    var windowpos = $(window).scrollTop() + topoffset;
     $('header nav li a').removeClass('active');
     $('.nav_right__item').removeClass('activve');
-    $('header a[href$="#maingallery"]').addClass('active');
-    $('.maingallery__item').addClass('activve');
-  }//windowpos
 
-  if (windowpos >= $('#about').offset().top){
-    $('header nav li a').removeClass('active');
-    $('.nav_right__item').removeClass('activve');
-    $('header a[href$="#about"]').addClass('active');
-    $('.about__item').addClass('activve');
-  }//windowpos
+    if (windowpos >= $('#maingallery').offset().top){
+      $('header nav li a').removeClass('active');
+      $('.nav_right__item').removeClass('activve');
+      $('header a[href$="#maingallery"]').addClass('active');
+      $('.maingallery__item').addClass('activve');
+    }//windowpos
 
-  if (windowpos >= $('#works').offset().top){
-    $('header nav li a').removeClass('active');
-    $('.nav_right__item').removeClass('activve');
-    $('header a[href$="#works"]').addClass('active');
-    $('.works__item').addClass('activve');
-  }//windowpos
+    if (windowpos >= $('#about').offset().top){
+      $('header nav li a').removeClass('active');
+      $('.nav_right__item').removeClass('activve');
+      $('header a[href$="#about"]').addClass('active');
+      $('.about__item').addClass('activve');
+    }//windowpos
 
-  if (windowpos >= $('#whyus').offset().top){
-    $('header nav li a').removeClass('active');
-    $('.nav_right__item').removeClass('activve');
-    $('header a[href$="#whyus"]').addClass('active');
-    $('.whyus__item').addClass('activve');
-  }//windowpos
+    if (windowpos >= $('#works').offset().top){
+      $('header nav li a').removeClass('active');
+      $('.nav_right__item').removeClass('activve');
+      $('header a[href$="#works"]').addClass('active');
+      $('.works__item').addClass('activve');
+    }//windowpos
 
-  if (windowpos >= $('#howwork').offset().top){
-    $('header nav li a').removeClass('active');
-    $('.nav_right__item').removeClass('active');
-    $('header a[href$="#howwork"]').addClass('active');
-    $('.howwork__item').addClass('active');
-  }//windowpos
+    if (windowpos >= $('#whyus').offset().top){
+      $('header nav li a').removeClass('active');
+      $('.nav_right__item').removeClass('activve');
+      $('header a[href$="#whyus"]').addClass('active');
+      $('.whyus__item').addClass('activve');
+    }//windowpos
 
-  if (windowpos >= $('#review').offset().top){
-    $('header nav li a').removeClass('active');
-    $('.nav_right__item').removeClass('activve');
-    $('header a[href$="#review"]').addClass('active');
-    $('.review__item').addClass('activve');
-  }//windowpos
+    if (windowpos >= $('#howwork').offset().top){
+      $('header nav li a').removeClass('active');
+      $('.nav_right__item').removeClass('activve');
+      $('header a[href$="#howwork"]').addClass('active');
+      $('.howwork__item').addClass('activve');
+    }//windowpos
 
-  if (windowpos >= $('#sendreview').offset().top){
-    $('header nav li a').removeClass('active');
-    $('.nav_right__item').removeClass('activve');
-    $('header a[href$="#sendreview"]').addClass('active');
-    $('.sendreview__item').addClass('activve');
-  }//windowpos
+    if (windowpos >= $('#review').offset().top){
+      $('header nav li a').removeClass('active');
+      $('.nav_right__item').removeClass('activve');
+      $('header a[href$="#review"]').addClass('active');
+      $('.review__item').addClass('activve');
+    }//windowpos
 
-});//window scroll
-
-
-
-
-
+    if (windowpos >= $('#sendreview').offset().top){
+      $('header nav li a').removeClass('active');
+      $('.nav_right__item').removeClass('activve');
+      $('header a[href$="#sendreview"]').addClass('active');
+      $('.sendreview__item').addClass('activve');
+    }//windowpos
+  });//window scroll
 });//on load
